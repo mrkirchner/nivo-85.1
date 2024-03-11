db77dbd7e8c8ff638fd4c67a630bfd00dd5f9ec9
Ben Kirchner <ben@mento.co>
2024-03-11 00:02:42 -0600

    Init

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    esmExternals: "loose",
+  },
+  transpilePackages: ["@nivo"], // https://github.com/plouc/nivo/issues/2310
 };
 
 export default nextConfig;
